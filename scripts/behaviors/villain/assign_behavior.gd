4b5112bb4227e00a4d0b692da3bd062d76f7d18e
David Zhang <davidz1729@gmail.com>
2026-09-23 00:04:08 -0700

    add pathfinding for moving towards civilian behavior

--- scripts/behaviors/villain/assign_behavior.gd
+++ scripts/behaviors/villain/assign_behavior.gd
@@ -18,8 +18,6 @@ func tick(_actor: Node, blackboard: Blackboard) -> int:
 	return SUCCESS
 
 func update_assigned_behavior(blackboard: Blackboard):
-	# Debug: always hunt heroes
-	var behavior = AssignBehavior.HUNT_HERO
-	#var behavior = AssignBehavior.HUNT_HERO if randi_range(0, 1) == 0 else AssignBehavior.HUNT_CIVILIAN
+	var behavior = AssignBehavior.HUNT_HERO if randi_range(0, 1) == 0 else AssignBehavior.HUNT_CIVILIAN
 	blackboard.set_value(AssignBehavior.BEHAVIOR_KEY, behavior)
 	blackboard.set_value(AssignBehavior.BEHAVIOR_UPDATED_TS, Time.get_ticks_msec())
--- scripts/behaviors/villain/move_toward_civilian.gd
+++ scripts/behaviors/villain/move_toward_civilian.gd
@@ -5,5 +5,7 @@ extends ActionLeaf
 func tick(actor: Node, blackboard: Blackboard) -> int:
 	var villain = actor as VillainTacticalEntity
 	var target_civilian: CivilianTacticalEntity = blackboard.get_value(IsCivilianDetected.DETECTED_CIV_KEY)
-	villain.velocity = villain.global_position.direction_to(target_civilian.global_position) * villain.entity_config.ground_speed
+	villain.navigation_agent.target_position = target_civilian.global_position
+	var next_path_pos: Vector2 = villain.navigation_agent.get_next_path_position()
+	villain.velocity = villain.global_position.direction_to(next_path_pos) * villain.entity_config.ground_speed
 	return RUNNING
